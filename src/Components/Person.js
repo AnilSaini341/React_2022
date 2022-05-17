7745df2d8db9fefbcc547e4aefb4f2a26c9a5baf
AnilSaini <Anil.Saini@mashvirtual.in>
2022-05-17 11:18:50 +0530

    Class Based Component

---
+++ src/Components/Person.js
@@ -0,0 +1,15 @@
+import React,{ Component } from "react";
+
+
+class Person extends Component {
+    render(){
+        return(
+            <div>
+            <div>Name:Anil Saini</div>
+            <div>Age:30 Years</div> <br></br>
+            </div>
+        )
+    }
+}
+
+export default Person;
