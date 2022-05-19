ab444d9f4162eb275b19ac633f016b043b882942
AnilSaini <Anil.Saini@mashvirtual.in>
2022-05-19 16:17:14 +0530

    Handling Form Inputs on Form Submit in Class Component

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,32 +5,49 @@ class Form extends Component {
     constructor(props) {
         super(props);
         this.props=props;
-        this.state={};
+        this.state={
+            name:null,
+            country:null,
+        };
     }
-    nameChangeHandler=(event)=>{
-        console.log("Name : "+event.target.value);
-    }
-    selectChange=(event)=>{
-        console.log("Select : "+event.target.value);
+
+    // nameChangeHandler=(event)=>{
+    //     console.log("Name : "+event.target.value);
+    // }
+
+    // selectChange=(event)=>{
+    //     console.log("Select : "+event.target.value);
+    // }
+    formSubmit=(event)=>{
+        event.preventDefault();   //Used for Stop page Refresh
+        console.log(this.state);
+        const name=event.target.name.value;
+        const country=event.target.country.value;
+        this.setState({name:name,country:country},()=>{
+            console.log(this.state);
+        });
     }
 
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.formSubmit}>
                     <div>
                         <label htmlFor="">Name</label> &nbsp;&nbsp;
-                        <input type="text" name="name" onKeyUp={this.nameChangeHandler}></input>
+                        {/* <input type="text" name="name" onKeyUp={this.nameChangeHandler}></input> */}
+                        <input type="text" name="name"></input>
                     </div>
                     <div>
                         <label htmlFor="">Select</label>&nbsp;&nbsp;
-                        <select onChange={this.selectChange}>
+                        <select name="country">
+                        {/* <select onChange={this.selectChange}> */}
                             <option value="1">1</option>
                             <option value="2">2</option>
                             <option value="3">3</option>
                             <option value="4">4</option>
                         </select>
                     </div>
+                    <button type='submit'>Submit</button>
                 </form>
             </div>
         );
