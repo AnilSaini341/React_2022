95653312af8df75594a13c3359a28c63e5ac2316
AnilSaini <Anil.Saini@mashvirtual.in>
2022-05-26 16:21:33 +0530

    Displaying Api fetch data

--- src/Components/DummyUsers.js
+++ src/Components/DummyUsers.js
@@ -6,24 +6,55 @@ class DummyUsers extends Component {
     constructor(props){
         super(props);
         this.state={
-
+            posts:null,
         }
     }
 
     componentDidMount(){
         axios.get("https://jsonplaceholder.typicode.com/posts")
         .then((response)=>{
-            console.log(response.data);
-        })
+            // console.log(response.data);
+            this.setState({posts:response.data},()=>
+                console.log(this.state));
+            })
         .catch((error)=>{
             console.log(error);
         });
     }
 
     render() {
+        let posts;
+        if(this.state.posts!=null){
+            posts=this.state.posts.map((post, index) =>{
+                return(
+                    // <h3 key={index}>S.No:- {index} | {post.title}, Age:{post.body} </h3>
+                    <table className="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>S. No</th>
+                        <th>UserId</th>
+                        <th>Title</th>
+                        <th>Info</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr key={index}>
+                        <td>{post.id}</td>
+                        <td>{post.userId}</td>
+                        <td>{post.title}</td>
+                        <td>{post.body}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                )
+            });
+        }
+        else{
+            posts =<h3 className='text-center m-5'>No Records Found</h3>;
+        }
         return (
             <div>
-                <h2 className='text-center'>Dummy Users Page</h2>
+                {posts}
             </div>
         );
     }
