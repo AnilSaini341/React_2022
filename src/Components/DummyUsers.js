5abc068b2db481ed6dae58cc549c67f557fbbbeb
AnilSaini <Anil.Saini@mashvirtual.in>
2022-05-26 16:44:51 +0530

    Deleting Data using Axios

--- src/Components/DummyUsers.js
+++ src/Components/DummyUsers.js
@@ -22,6 +22,17 @@ class DummyUsers extends Component {
         });
     }
 
+    deleteHandler = (id) => {
+        // alert(id);
+        axios.delete("https://jsonplaceholder.typicode.com/posts/"+id)
+        .then((response)=>{
+             console.log(response);
+            })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     render() {
         let posts;
         if(this.state.posts!=null){
@@ -35,6 +46,7 @@ class DummyUsers extends Component {
                         <th>UserId</th>
                         <th>Title</th>
                         <th>Info</th>
+                        <th>Actions</th>
                       </tr>
                     </thead>
                     <tbody>
@@ -43,6 +55,8 @@ class DummyUsers extends Component {
                         <td>{post.userId}</td>
                         <td>{post.title}</td>
                         <td>{post.body}</td>
+                        <td><button type="button" className='btn btn-danger' 
+                        onClick={()=>this.deleteHandler(post.id)}>Delete</button></td>
                       </tr>
                     </tbody>
                   </table>
