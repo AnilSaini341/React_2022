9d2d014c894a7282c0e55bedce6fb0dd881b1561
AnilSaini <Anil.Saini@mashvirtual.in>
2022-05-26 13:26:18 +0530

    useState hook to handle state in functional component

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 function Homepage(props) {
+  const[name,setName]=useState("Anil ");
     return (
         <div>
             <div className="container-fluid bg-2 text-center">
   <h3>{props.pagename}</h3>
+  <button type='button' className="btn btn-primary m-3" onClick={()=>{setName("Anil Saini")}}>Change {name}</button>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
 </div>
 
